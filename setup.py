fdc650223a79f10e3b8c88c80e1ea6a1f946276e
tc-imba <liuyh615@sjtu.edu.cn>
2020-08-17 21:52:29 +0800

    publish to pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,24 @@ tc-imba <liuyh615@sjtu.edu.cn>
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='oauth_jaccount',
-    version='0.1.0',
+    version='0.1.1',
+    author="tc-imba",
+    author_email="liuyh615@126.com",
+    description="python package for jaccount oauth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tc-imba/python-oauth-jaccount",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
     packages=['oauth_jaccount'],
     include_package_data=True,
     install_requires=[
